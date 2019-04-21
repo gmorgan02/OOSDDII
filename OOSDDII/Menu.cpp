85d1d2edd95b75e62fdc486563cd60bf75a3d6da
Unknown <georgecranmermorgan@outlook.com>
2019-04-21 17:42:30 +0100

    Recreated DB with Primary and autoincrement ID

--- OOSDDII/Menu.cpp
+++ OOSDDII/Menu.cpp
@@ -2,7 +2,33 @@
 
 void Menu()
 {
+	int option;
+	bool validChoice = false;
+	std::cout << "Welcome to the program!\n";
 	
 
-	std::cout << "Welcome to the program!\n";
+
+	while (!validChoice)
+	{
+		std::cout << "Please Select from one of the following:\n";
+		std::cout << "1. Patient Login\n2. Doctor Login\n3. Exit Program\n";
+		std::cin >> option;
+		switch (option)
+		{
+		case 1:
+			validChoice = true;
+			std::cout << "Option 1\n";
+			break;
+		case 2:
+			validChoice = true;
+			std::cout << "Option 1\n";
+			break;
+		case 3:
+			validChoice = true;
+			break;
+		default:
+			std::cout << "Please select a valid option\n\n";
+			break;
+		}
+	}	
 }
