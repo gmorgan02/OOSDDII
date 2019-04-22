5f4734c650c2701835728df97e31b73312391abd
Unknown <georgecranmermorgan@outlook.com>
2019-04-22 20:57:15 +0100

    Removed Sqlite

--- OOSDDII/DoctorLogin.cpp
+++ OOSDDII/DoctorLogin.cpp
@@ -1,35 +1,9 @@
 #include <iostream>
-#include "sqlite3.h"
+
+
 
 bool ValidateLogin(std::string, std::string)
-{
-	sqlite3 * connection = nullptr;
-	int result = sqlite3_open("OOSDDII.db", &connection);
-
-	if (SQLITE_OK != result)
-	{
-		std::cout << "\n" << sqlite3_errmsg(connection);
-		sqlite3_close(connection);
-		return result;
-	}
-
-	sqlite3_stmt* query = nullptr;
-	result = sqlite3_prepare_v2(connection, "SELECT Name, Password FROM Doctors", -1, &query, nullptr);
-
-	if (SQLITE_OK != result)
-	{
-		std::cout << "\n" << sqlite3_errmsg(connection);
-		sqlite3_close(connection);
-		return result;
-	}
-
-	while (SQLITE_ROW == sqlite3_step(query))
-	{
-		std::cout << "\n" << sqlite3_column_text(query, 0);
-	}
-
-	sqlite3_finalize(query);
-	sqlite3_close(connection);
+{	
 
 	return false;
 }
